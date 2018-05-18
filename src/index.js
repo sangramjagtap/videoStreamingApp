150e8a3733d966e667920ec2c18a2ca176770b24
Sangram Jagtap <sangram.jagtap@staples.com>
2018-05-18 17:52:20 +0530

    Added App component

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = function() {
+    return <div>HELLO THERE!</div>;
+}
+
+ReactDOM.render(<app />, document.querySelector('.container'));
